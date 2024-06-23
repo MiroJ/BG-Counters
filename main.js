642d748d211d5e906880e91348f0f67e124200f7
Miro Jeliaskoff <jeliaskoff@hotmail.com>
2024-06-23 14:25:32 +0300

    Initial commit.

---
+++ main.js
@@ -0,0 +1,37 @@
+const startDate = new Date('2023-01-01');
+const annualAmount = ((9.145 * 53200) / (24 * 60 * 60)) * 1.83;
+const amountNrrp = 1_300_000_000;
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const divNppr = document.getElementById('amount-nrrp');
+    if (divNppr) {
+        divNppr.innerHTML = amountNrrp.toLocaleString();
+    }
+
+    setInterval(() => {
+        const annualAmount = calculateAnnualAmount();
+
+        const divCounter = document.getElementById('amount-botas');
+        if (divCounter) {
+            divCounter.innerHTML = annualAmount.toLocaleString();
+        }
+
+        const divMedication = document.getElementById('botas-medication');
+        if (divMedication) {
+            const medicationUnits = calculateMedicationUnits(annualAmount);
+            divMedication.innerHTML = medicationUnits.toLocaleString();
+        }
+    }, 200);
+
+    const calculateAnnualAmount = () => {
+        const now = new Date();
+        const elapsedSeconds = Math.abs(now.getTime() - startDate.getTime()) / 1000;
+
+        return Math.round(annualAmount * elapsedSeconds);
+    };
+
+    const calculateMedicationUnits = (amount) => {
+        return Math.round(amount / 17.5);
+    };
+
+});
