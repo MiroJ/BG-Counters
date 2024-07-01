0fea37d421aa86c8a078a5fd3b4a73e20c102174
Miro Jeliaskoff <jeliaskoff@hotmail.com>
2024-07-01 11:23:21 +0300

    Added a widget example.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const startDate = new Date('2023-01-01');
-const amountPerDay = 9.145 * 53200 * 1.83; // leva per day
-const amountPerSecond = amountPerDay / (24 * 60 * 60); // leva per second
+const startDateBotas = new Date('2023-01-01');
+const amountBotasPerDay = 9.145 * 53_200 * 1.83; // leva per day
+const amountPerSecond = amountBotasPerDay / (24 * 60 * 60); // leva per second
 const amountNrrp = 1_300_000_000;
 const bgPopulation = 6_519_789;
 
@@ -20,13 +20,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Set amount for pensioners
     const divBotasPensioners = document.getElementById('botas-pensioners');
     if (divBotasPensioners) {
-        const pensioners = Math.round((amountPerDay * 365) / 500000);
+        const pensioners = Math.round((amountBotasPerDay * 365) / 500_000);
         divBotasPensioners.innerHTML = pensioners.toLocaleString();
     }
     // Set amount for pensioners
     const divNrrpPensioners = document.getElementById('nnrp-pensioners');
     if (divNrrpPensioners) {
-        const pensioners = Math.round(amountNrrp / 2100000);
+        const pensioners = Math.round(amountNrrp / 2_100_000);
         divNrrpPensioners.innerHTML = pensioners.toLocaleString();
     }
     // Set amount per capita
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Set the amount of medication
         const divMedication = document.getElementById('botas-medication');
         if (divMedication) {
-            const medicationUnits = calculateMedicationUnits(currentBotasAmount);
+            const medicationUnits = Math.round(currentBotasAmount / 17.5);
             divMedication.innerHTML = medicationUnits.toLocaleString();
         }
         // Set the amount of sidewalks
@@ -66,12 +66,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const calculateBotasAmounts = () => {
         const now = new Date();
-        const elapsedSeconds = Math.abs(now.getTime() - startDate.getTime()) / 1000;
+        const elapsedSeconds = Math.abs(now.getTime() - startDateBotas.getTime()) / 1000;
 
         return Math.round(amountPerSecond * elapsedSeconds);
     };
 
-    const calculateMedicationUnits = (amount) => {
-        return Math.round(amount / 17.5);
-    };
 });
